c5002205275b0737835f886af3b2cd2e90d17e0e
코린이 <139521789+suemminPark@users.noreply.github.com>
2023-12-04 10:49:47 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    answer = 0
+    ans1 = ''
+    ans2 = ''
+    ans1 = int(str(a) + str(b))
+    ans2 = int(str(b) + str(a))
+    if ans1 > ans2:
+        answer = ans1
+    else:
+        answer = ans2
+    
+    return answer
